69ad6b2eaaf8ded28f767415424c115da5c6ea4c
MajedHoussen <mhoussenvv@hotmail.com>
2020-09-30 19:15:12 +0300

    New Version...

--- lib/Address/addAddress.dart
+++ lib/Address/addAddress.dart
@@ -1,24 +1,150 @@
 import 'package:e_shop/Config/config.dart';
 import 'package:e_shop/Widgets/customAppBar.dart';
 import 'package:e_shop/Models/address.dart';
+import 'package:e_shop/Widgets/myDrawer.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddAddress extends StatelessWidget {
+  final formKey = GlobalKey<FormState>();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final cName = TextEditingController();
+  final cPhoneNumber = TextEditingController();
+  final cFlatHomeNumber = TextEditingController();
+  final cCity = TextEditingController();
+  final cState = TextEditingController();
+  final cPinCode = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        key: scaffoldKey,
+        appBar: MyAppBar(),
+        drawer: MyDrawer(),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            if (formKey.currentState.validate()) {
+              final model = AddressModel(
+                name: cName.text.trim(),
+                state: cState.text.trim(),
+                pincode: cPinCode.text,
+                city: cCity.text.trim(),
+                phoneNumber: cPhoneNumber.text,
+                flatNumber: cFlatHomeNumber.text,
+              ).toJson();
+
+              //addToFireStore
+              EcommerceApp.fireStore
+                  .collection(EcommerceApp.collectionUser)
+                  .document(EcommerceApp.sharedPreferences
+                      .getString(EcommerceApp.userUID))
+                  .collection(EcommerceApp.subCollectionAddress)
+                  .document(DateTime.now().millisecondsSinceEpoch.toString())
+                  .setData(model)
+                  .then((value) {
+                final snack =
+                    SnackBar(content: Text("New Address added successfully."));
+                scaffoldKey.currentState.showSnackBar(snack);
+                FocusScope.of(context).requestFocus(FocusNode());
+                formKey.currentState.reset();
+              });
+            }
+          },
+          label: Text("Done"),
+          backgroundColor: Colors.blue[700],
+          icon: Icon(Icons.check),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Center(
+                    child: ShaderMask(
+                      shaderCallback: (b) => RadialGradient(
+                        center: Alignment.topLeft,
+                        radius: 1,
+                        colors: [Colors.blue[700], Colors.blue[200]],
+                        tileMode: TileMode.mirror,
+                      ).createShader(b),
+                      child: const Text(
+                        "Add New Address",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    MyTextField(
+                      hint: "Name",
+                      controller: cName,
+                    ),
+                    MyTextField(
+                      hint: "Phone number",
+                      controller: cPhoneNumber,
+                    ),
+                    MyTextField(
+                      hint: "Flat Number / House Number",
+                      controller: cFlatHomeNumber,
+                    ),
+                    MyTextField(
+                      hint: "City",
+                      controller: cCity,
+                    ),
+                    MyTextField(
+                      hint: "State / Country",
+                      controller: cState,
+                    ),
+                    MyTextField(
+                      hint: "Pin code",
+                      controller: cPinCode,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
 class MyTextField extends StatelessWidget {
+  final String hint;
+  final TextEditingController controller;
+
+  const MyTextField({Key key, this.hint, this.controller}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-
+      padding: EdgeInsets.all(8),
+      child: Container(
+        margin: EdgeInsets.only(right: 20),
+        padding: EdgeInsets.all(15),
+        width: MediaQuery.of(context).size.width * 1,
+        height: 50,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: Colors.blue[200])),
+        child: TextFormField(
+          controller: controller,
+          decoration: InputDecoration.collapsed(
+              hintText: hint, hintStyle: TextStyle(color: Colors.blue[300])),
+          validator: (val) => val.isEmpty ? "Field can not be empty" : null,
+        ),
+      ),
     );
   }
 }
